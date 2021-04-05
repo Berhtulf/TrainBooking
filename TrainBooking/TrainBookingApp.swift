99c0f456556c86a240ce885e96f36001066c80bc
Václavík Martin <mvaclavik96@icloud.com>
2021-04-05 13:29:01 +0200

    Initial Commit

---
+++ TrainBooking/TrainBookingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrainBookingApp.swift
+//  TrainBooking
+//
+//  Created by Martin Václavík on 05.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct TrainBookingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
